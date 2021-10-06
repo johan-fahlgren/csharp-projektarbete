23fc075364cca03190265a8b811035a393f5851f
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2021-10-06 13:15:43 +0200

    #1 - [SPIKE/GREEN] - interface implementation & Test_MakeWithdrawal
    Testade att skapa en interface och det gick bra så vi behåller den.

---
+++ Account/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class SavingsAccount : IPayer
+    {
+        private int _balance;
+
+        public SavingsAccount(int balance)
+        {
+            _balance = balance;
+        }
+
+        public bool TryMakeWithdrawal(int amount)
+        {
+            bool canWithdrawal = amount <= _balance;
+
+            if (canWithdrawal)
+            {
+                _balance -= amount;
+            }
+
+            return canWithdrawal;
+        }
+
+        
+
+    }
+}
--- Tests/AccountTests.cs
+++ Tests/AccountTests.cs
@@ -18,30 +18,45 @@ namespace Tests
         }
 
 
-        //TESTS SAVING ACCOUNTS
+        //TESTS SAVINGS ACCOUNT
         [Fact]
         public void Test_CreateSavingsAccount()
         {
+           
             _account.CreateAccount("savings_Account");
             
+
             Assert.Equal("savings_Account", _account.NewAccount);
+
         }
 
 
         [Fact]
         public void Test_DepositMoney()
         {
-          
+         
             bool noMoneyDeposit = _account.DepositMoney(0);
             bool newMoneyDeposit = _account.DepositMoney(1000);
-            
-           
+                  
 
             Assert.False(noMoneyDeposit);
             Assert.True(newMoneyDeposit);
             
         }
 
+        [Fact]
+        public void Test_MakeWithdrawal()
+        {
+            SavingsAccount account = new SavingsAccount(1000);
+
+            //Withdrawal with money left
+            var withdrawalMoney = account.TryMakeWithdrawal(250);
+            Assert.True(withdrawalMoney);
+
+            //Overdraft withdrawal
+            withdrawalMoney = account.TryMakeWithdrawal(800);
+            Assert.False(withdrawalMoney);
+        }
 
     }
 }
